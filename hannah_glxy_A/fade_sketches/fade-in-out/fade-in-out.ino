6935acbb2c3a98240d63d9834fdc80e4ad09d711
sleethma <sleeth.ma@gmail.com>
2019-09-14 23:19:29 -0500

    fully dynamic indiv light control

--- hannah_glxy_A/fade_sketches/fade-in-out/fade-in-out.ino
+++ hannah_glxy_A/fade_sketches/fade-in-out/fade-in-out.ino
@@ -23,10 +23,13 @@ CRGB leds[NUM_LEDS];
 #define PIN 4 // Output Pin to Data Line on Strip
 #define COLOR_ORDER GRB  // I had to change this for my strip if your color is off then you know.
 int fadeAmount10 = 1;  // Set the amount to fade I usually do 5, 10, 15, 20, 25 etc even up to 255.
-int fadeAmount15 = 1;  // Set the amount to fade I usually do 5, 10, 15, 20, 25 etc even up to 255.
+int fadeAmount15 = 25;  // Set the amount to fade I usually do 5, 10, 15, 20, 25 etc even up to 255.
+int fadeAmount13 = 5;  // Set the amount to fade I usually do 5, 10, 15, 20, 25 etc even up to 255.
+
 
 int brightness = 0; 
 int brightness10 = 0;
+int brightness13 = 0;
 int brightness15 = 0;
 
 void setup()
@@ -36,27 +39,25 @@ void setup()
 
 void loop()
 { 
-  pulse10(10,75,75,75, 10, &brightness10);
-  pulse10(15, 180,180,180,  10, &brightness15);
-  //  for(int i = 0; i < NUM_LEDS; i++ )
-  //  {
-  //  leds[i].setRGB(50,255,250);  // Set Color HERE!!!
-  //  leds[i].fadeLightBy(brightness);
-  // }
+  pulse10(10,75,75,75, 2, &brightness10, &fadeAmount10);
+  
+  pulse10(12,75,75,180, 2, &brightness13, &fadeAmount13);
+
+  pulse10(15, 0,180,0,2, &brightness15, &fadeAmount15);
   
   FastLED.show();
 
 }
 
-void pulse10(int num, int red, int green, int blue,  int delayTime, int *brightness10P){
+void pulse10(int num, int red, int green, int blue,  int delayTime, int *brightness10P, int *fadeAmount10){
 leds[num].setRGB(red,green,blue);  // Set Color HERE!!!
-  *brightness10P = *brightness10P + fadeAmount10;
+  *brightness10P = *brightness10P + *fadeAmount10;
   // reverse the direction of the fading at the ends of the fade: 
   if(*brightness10P < 1 || *brightness10P > 254)
   {
-    fadeAmount10 = -fadeAmount10 ; 
+    *fadeAmount10 = -*fadeAmount10; 
   }    
-  delay(25);  // This delay sets speed of the fade. I usually do from 5-75 but you can always go higher.
+  delay(delayTime);  // This delay sets speed of the fade. I usually do from 5-75 but you can always go higher.
   leds[num].fadeLightBy(*brightness10P);
 }
 
